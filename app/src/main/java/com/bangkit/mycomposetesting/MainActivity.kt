741a8b3488c2b4c45b97b2a151bbfe2ac4210fb1
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-11-14 20:39:36 +0700

    first commit

---
+++ app/src/main/java/com/bangkit/mycomposetesting/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.bangkit.mycomposetesting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.bangkit.mycomposetesting.ui.CalculatorApp
+import com.bangkit.mycomposetesting.ui.theme.MyComposeTestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeTestingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalculatorApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_3A)
+@Composable
+fun Preview() {
+    MyComposeTestingTheme {
+        CalculatorApp()
+    }
+}
